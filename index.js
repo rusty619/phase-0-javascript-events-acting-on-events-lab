9b1c02fe7f249453f2a05dcfe7c4a714691c63d8
rusty619 <rusty6916@gmail.com>
2022-03-20 14:51:07 -0400

    commit tree cc0fc60185b081c35fcd79523aeef3801a56fb8a

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+let moveDodgerLeft = () => {
+        const leftNumbers = dodger.style.left.replace("px", "");
+        const left = parseInt(leftNumbers, 10);
+
+       if(left > 0){
+           dodger.style.left = `${left - 1}px`;
+       }
+}
+
+let moveDodgerRight = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+const dodger= document.getElementById("dodger")
+dodger.style.backgroundColor = '#FF69B4'
+//dodger.style.bottom = '0px'
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft"){
+        moveDodgerLeft()
+    }
+    if(event.key === 'ArrowRight'){
+        moveDodgerRight()
+    }
+});
+
+
